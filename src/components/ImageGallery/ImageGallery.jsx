88124627b7044b46fd25fd40499a1e4c29a2d39b
Bohdan <b.orlovskiy@gmail.com>
2022-11-27 13:46:40 +0200

    commit

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,54 +1,50 @@
-// Разметка галереи изображений по http-запросу
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { ImageGalleryItem } from './ImageGalleryItem';
 import { Box } from '../SearchResult.styled';
 
-export default class ImageGallery extends Component {
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    images: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        largeImageURL: PropTypes.string.isRequired,
-        tags: PropTypes.string.isRequired,
-      })
-    ),
+const ImageGallery = ({ images, onClick }) => {
+  const handleClickFromItem = largeImage => {
+    onClick(largeImage);
   };
 
-  handleClickFromItem = largeImage => {
-    this.props.onClick(largeImage);
-  };
+  return (
+    // as="ul": div --> ul
+    <Box
+      display="grid"
+      gridGap={4}
+      gridTemplateColumns="repeat(auto-fill, minmax(400px, 1fr))"
+      mt={0}
+      mb={0}
+      p={0}
+      ml="auto"
+      mr="auto"
+      bg="white"
+      maxWidth="calc(100vw - 48px)"
+      list-style="none"
+      as="ul"
+    >
+      {images.map(({ id, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          tags={tags}
+          largeImageURL={largeImageURL}
+          onClick={handleClickFromItem}
+        />
+      ))}
+    </Box>
+  );
+};
 
-  render() {
-    const { images } = this.props;
+ImageGallery.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    })
+  ),
+};
 
-    return (
-      // as="ul": div --> ul
-      <Box
-        display="grid"
-        gridGap={4}
-        gridTemplateColumns="repeat(auto-fill, minmax(400px, 1fr))"
-        mt={0}
-        mb={0}
-        p={0}
-        ml="auto"
-        mr="auto"
-        bg="white"
-        maxWidth="calc(100vw - 48px)"
-        list-style="none"
-        as="ul"
-      >
-        {images.map(({ id, largeImageURL, tags }) => (
-          <ImageGalleryItem
-            key={id}
-            tags={tags}
-            largeImageURL={largeImageURL}
-            onClick={this.handleClickFromItem}
-          />
-        ))}
-      </Box>
-    );
-  }
-}
+export default ImageGallery;
